988379bbfc2567f58653c41bf962b1dbae580c06
Adam Kearney <aklives@hotmail.com>
2019-03-18 23:51:36 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,7 +35,7 @@ end
 
 def selective_tweet_shortener array_of_tweets
   array_of_tweets.each do |tweet|
-    if tweet.length < 140
+    if tweet.length > 140
       puts word_substituter(tweet)
     end
   end
